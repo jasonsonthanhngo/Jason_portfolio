27dd235bae677bddee69f7a6db8538e4b08da0e5
Jason Ngo <131301318+jasonsonthanhngo@users.noreply.github.com>
2023-08-14 22:29:59 -0700

    Add files via upload

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,7 +1,33 @@
+'use client';
+
 import React from 'react';
 import { AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
+import { useState, useEffect } from 'react';
 
 const ContactSection = () => {
+	const [iconSize, setIconSize] = useState(500); // Default size
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth <= 1500) {
+				// Adjust the breakpoint as needed
+				setIconSize(window.innerWidth * 0.3);
+			} else {
+				setIconSize(450);
+			}
+		};
+
+		// Initial call
+		handleResize();
+
+		// Event listener for window resize
+		window.addEventListener('resize', handleResize);
+
+		// Cleanup the event listener
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 	return (
 		<section id="contact">
 			<div className=" my-12 pb-12 md:pt-16 md:pb-48">
@@ -9,7 +35,7 @@ const ContactSection = () => {
 					Contact Me
 					<hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
 				</h1>
-				<div className="mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+				<div className="mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-center">
 					<div className="flex flex-col md:flex-row items-center justify-center space-x-2 mb-1 md:space-x-4">
 						<a
 							href="https://github.com/jasonsonthanhngo"
@@ -17,7 +43,7 @@ const ContactSection = () => {
 							target="_blank">
 							<AiOutlineGithub
 								className="hover:-translate-y-5 transition-transform cursor-pointer text-teal-500 dark:text-teal-100"
-								size={500}
+								size={iconSize}
 							/>
 						</a>
 
@@ -27,7 +53,7 @@ const ContactSection = () => {
 							target="_blank">
 							<AiOutlineLinkedin
 								className="hover:-translate-y-5 transition-transform cursor-pointer text-teal-500 dark:text-teal-100"
-								size={500}
+								size={iconSize}
 							/>
 						</a>
 					</div>
