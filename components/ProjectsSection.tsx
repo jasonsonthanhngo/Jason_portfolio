a1eba47cbb8af04aaee26e560b1dd606f28b075b
Jason Ngo <131301318+jasonsonthanhngo@users.noreply.github.com>
2023-08-26 21:55:03 -0700

    Update ProjectsSection.tsx

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -17,7 +17,7 @@ const projects = [
 		name: 'Companion AI',
 		description:
 			'An application crafted with Next.js and powered by OpenAI API. Shape personalized chatbots by infusing personas of favorites. Train and seed AI companions seamlessly for lifelike interactions!',
-		image: '/AI_Homepage.PNG',
+		image: '/Companion-AI-Homepage.png',
 		github: 'https://github.com/jasonsonthanhngo/ai-companion',
 		link: 'https://jason-ai-companion.vercel.app/',
 	},
